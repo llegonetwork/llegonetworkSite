a514327edb4887a92ac302ff9fa0587321b6af61
LegoLandon7 <legolandon25@gmail.com>
2026-07-06 17:07:19 -0400

    lowk this could work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
-    base: process.env.GITHUB_ACTIONS ? "llegonetworkSite" : "/",
+    plugins: [react()]
   };
 });
