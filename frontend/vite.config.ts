4c7657a3c663a2e695abf5f6c027fd0a8e705e47
LegoLandon7 <legolandon25@gmail.com>
2026-07-06 17:06:08 -0400

    PLEASE WORK

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: env.BASE_URL,
+    base: process.env.GITHUB_ACTIONS ? "llegonetworkSite" : "/",
   };
 });
